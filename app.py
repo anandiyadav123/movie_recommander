dfeca1795fa0b27eecf3def6b691cdbef3be8861
anandiyadav123 <anandiyadavrkt006@gmail.com>
2025-07-26 18:45:19 +0530

    Implement movie poster fetching from TMDB API and update recommendation logic in app.py

--- app.py
+++ app.py
@@ -1,10 +1,80 @@
+# import pickle
+# import streamlit as st
+# import requests
+
+# def fetch_poster(movie_id):
+#     # Always return placeholder image (no API call)
+#     return "https://via.placeholder.com/500x750?text=No+Image"
+
+# def recommend(movie):
+#     index = movies[movies['title'] == movie].index[0]
+#     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+#     recommended_movie_names = []
+#     recommended_movie_posters = []
+#     for i in distances[1:6]:
+#         # fetch the movie poster
+#         movie_id = movies.iloc[i[0]].movie_id
+#         recommended_movie_posters.append(fetch_poster(movie_id))
+#         recommended_movie_names.append(movies.iloc[i[0]].title)
+
+#     return recommended_movie_names,recommended_movie_posters
+
+
+# st.header('Movie Recommender System')
+# movies = pickle.load(open('model/movie_list.pkl','rb'))
+# similarity = pickle.load(open('model/similarity.pkl','rb'))
+
+# movie_list = movies['title'].values
+# selected_movie = st.selectbox(
+#     "Type or select a movie from the dropdown",
+#     movie_list
+# )
+
+# if st.button('Show Recommendation'):
+#     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+#     col1, col2, col3, col4, col5 = st.columns(5)
+#     with col1:
+#         st.text(recommended_movie_names[0])
+#         st.image(recommended_movie_posters[0])
+#     with col2:
+#         st.text(recommended_movie_names[1])
+#         st.image(recommended_movie_posters[1])
+
+#     with col3:
+#         st.text(recommended_movie_names[2])
+#         st.image(recommended_movie_posters[2])
+#     with col4:
+#         st.text(recommended_movie_names[3])
+#         st.image(recommended_movie_posters[3])
+#     with col5:
+#         st.text(recommended_movie_names[4])
+#         st.image(recommended_movie_posters[4])
+
+
 import pickle
 import streamlit as st
 import requests
+import urllib3
+
+# Placeholder image URL for failed poster fetches
+PLACEHOLDER_POSTER = "https://via.placeholder.com/500x750?text=No+Image"
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def fetch_poster(movie_id):
-    # Always return placeholder image (no API call)
-    return "https://via.placeholder.com/500x750?text=No+Image"
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    try:
+        data = requests.get(url, timeout=5, verify=False)  # Disable SSL verification
+        data.raise_for_status()
+        data = data.json()
+        poster_path = data.get('poster_path')
+        if poster_path:
+            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+            return full_path
+        else:
+            return PLACEHOLDER_POSTER
+    except Exception:
+        return PLACEHOLDER_POSTER
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
@@ -17,7 +87,7 @@ def recommend(movie):
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names, recommended_movie_posters
 
 
 st.header('Movie Recommender System')
@@ -31,7 +101,7 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names[0])
@@ -49,8 +119,6 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
-
-
 
 
 
